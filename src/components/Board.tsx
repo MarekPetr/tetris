7aa688e74ebe4cbc5e04b52e6191a06fc8fc236c
Petr Marek <petr.marek18@gmail.com>
2025-11-18 10:05:54 +0100

    feat: make cell size responsive

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,23 +1,35 @@
+import { useState, useEffect } from 'react'
 import { Shape } from 'game/shape'
 
 const CELL_COLOR = '#1A101B'
 
-const Cell = ({filled, color}: {filled: boolean, color: string}) => {
+const Cell = ({filled, color, boardHeight}: {filled: boolean, color: string, boardHeight: number}) => {
+  const [height, setHeight] = useState((window.innerHeight / boardHeight) * 0.6)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight((window.innerHeight / boardHeight) * 0.6)
+    }
+    
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [boardHeight])
+
   return (
-    <div className='cell' style={{backgroundColor: filled ? color : CELL_COLOR}}/>
+    <div className='cell' style={{backgroundColor: filled ? color : CELL_COLOR, height: height, width: height}}/>
   )
 }
   
-const Row = ({size, index, shapes}: {size: number, index: number, shapes: Shape[]}) => {
+const Row = ({size, index, shapes, boardHeight}: {size: number, index: number, shapes: Shape[], boardHeight: number}) => {
   let row = []
   for (let i = 0; i < size; i ++) {
     const position = index + i
     const shape = shapes.find((shape) => shape.indexes.includes(position))
     let element
     if (shape) {
-      element = <Cell filled={true} color={shape.color} key={position}/>
+      element = <Cell filled={true} color={shape.color} boardHeight={boardHeight} key={position}/>
     } else {
-      element = <Cell filled={false} color={CELL_COLOR} key={position}/>
+      element = <Cell filled={false} color={CELL_COLOR} boardHeight={boardHeight} key={position}/>
     }
     row.push(element)
   }
@@ -30,7 +42,7 @@ const Board = ({height, width, shapes}: {height: number, width: number, shapes: 
   let rows = []
   for (let i = 0; i < height*width; i+=width) {
     rows.push(
-      <Row size={width} index={i} shapes={shapes} key={i}/>
+      <Row boardHeight={height} size={width} index={i} shapes={shapes} key={i}/>
     )
   }
   return (
